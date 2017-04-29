0fdd69012ef1369ed99a0779370d11a9b68502d8
András Nemes <nemesa1@gmail.com>
2017-04-29 20:07:14 +0200

    First commit

---
+++ PPBF/R/PPBF.R
@@ -0,0 +1,94 @@
+#' PPBF.ParseDataFromRawPlateData
+#'
+#' @param rowNumber number of rows fo the plate
+#' @param columnNumber number of the usefull data columns on the plate (first column is the name)
+#' @param vectorWithRawData raw data separated with ','
+#' @return a DataFrame with the parsed data
+#' @examples
+#' PPBF.ParseDataFromRawPlateData(2,2,c("A,1,2","B,1,2"))
+
+PPBF.ParseDataFromRawPlateData<-function(rowNumber,columnNumber,vectorWithRawData){
+  parsedData_DataFrame=data.frame(X00=c(0))
+  
+  #create empty dataframe columns
+  for(columnIndex in 1:columnNumber){ 
+    coumnName<-"X"
+    if(columnIndex<10)
+    {
+      #if the actual column is in the first 10 than we add a '0' char to X, so at the end insted of 'X2' it will be 'X02'
+      coumnName=paste(coumnName, "0", sep="") #>concat....
+    }
+    coumnName=paste(coumnName, columnIndex, sep="")
+    parsedData_DataFrame[coumnName]=c(0)  
+  }
+  
+  for(row in 1:rowNumber){
+    #split the line by "," character
+    parsedRow <- unlist(strsplit(vectorWithRawData[row], ",")) 
+    for(column in 1:(columnNumber+1)){ 
+      #set each column one by one in the result dataframe from the parsedRow
+      parsedData_DataFrame[row,column]<-parsedRow[column]
+    }
+  }
+  
+  return(parsedData_DataFrame)
+}
+
+#' PPBF.Run
+#'
+#' @param filepath The file path for the csv file (eg: c:/Work/PrestoBlue_Sample.csv)
+#' @param printInfo default(FALSE) set this TRUE to display information messages during processing
+#' @param numberOfRowsPrefixString default("Number of rows,,,,") a string which will be in the raw input file every char after this will be parsed to a number
+#' @param numberOfColumnsPrefixString default("Number of columns,,,,") a string which will be in the raw input file every char after this will be parsed to a number
+#' @param dataStartsAfterThisString default("Results for Meas A -  (RFU)") the funtion will read the raw data after this line
+#' @return a list(RowNum=(number of rows of the plate),ColNum=(number of coulmns of the plate),Data= (a DataFram with the data)
+#' @examples
+#' parseResult <- PPBF.Run("c:/WORK/PrestoBlue_Sample.csv")
+#' 
+#' parseResult <- PPBF.Run("c:/WORK/PrestoBlue_Sample.csv",printInfo=TRUE)
+#' 
+#' rowNumber <- parseResult$RowNum
+#' columnNumber <- parseResult$ColNum
+#' dataFrame <- parseResult$Data
+
+PPBF.Run <- function(filepath
+                                              , printInfo=FALSE
+                                              , numberOfRowsPrefixString="Number of rows,,,,"
+                                              , numberOfColumnsPrefixString="Number of columns,,,,"
+                                              , dataStartsAfterThisString="Results for Meas A -  (RFU)") {
+  
+  fileConnection = file(filepath, "r") #open for read
+  allLinesInFile=c()
+  numberOfLinesInTheFile<-0
+  #read all lines into the allLinesInFile vector
+  while ( TRUE ) {
+    line = readLines(fileConnection, n = 1) #read one line
+    if ( length(line) == 0 ) { 
+      break #exit if end of file
+    }
+    allLinesInFile[numberOfLinesInTheFile]<-line
+    numberOfLinesInTheFile<-numberOfLinesInTheFile+1
+  }
+  #close file
+  close(fileConnection) 
+  
+  if(printInfo){print("Line wich contains the number of rows:"); print(allLinesInFile[startsWith(allLinesInFile,numberOfRowsPrefixString)])}
+  numberOfRows<-as.numeric(substring(allLinesInFile[startsWith(allLinesInFile,numberOfRowsPrefixString)],nchar(numberOfRowsPrefixString)+1))
+  if(printInfo){print("Line wich contains the number of columns:"); print(allLinesInFile[startsWith(allLinesInFile,numberOfColumnsPrefixString)])}
+  numberOfColumns<-as.numeric(substring(allLinesInFile[startsWith(allLinesInFile,numberOfColumnsPrefixString)],nchar(numberOfColumnsPrefixString)+1))
+  #seach for the data in the file
+  dataStartsAt<-(which(allLinesInFile == dataStartsAfterThisString)+2)
+  if(printInfo){print("data starts at line:"); print(dataStartsAt)}
+  indexesWhereTheDataIs<-dataStartsAt:(dataStartsAt+numberOfRows-1) #normaly this is 7:14 (from line 7 to line 14)
+  
+  if(printInfo){print("Raw data lines:"); print(allLinesInFile[indexesWhereTheDataIs])}
+  
+  if(printInfo){print("All Lines in the file:"); print(allLinesInFile)}
+  
+  result<-list(RowNum=numberOfRows,
+			   ColNum=numberOfColumns,
+			   Data=PPBF.ParseDataFromRawPlateData(numberOfRows,numberOfColumns,allLinesInFile[indexesWhereTheDataIs]))
+  
+  return(result)
+}
+
---
+++ PPBF/README.md
@@ -0,0 +1,69 @@
+﻿# PPBF - ProcessPrestoBlueFiles
+R package to process result files from a Plate Reader
+
+## Install and use devtools
+``` r
+install.packages("devtools")
+library(devtools)
+```
+
+## Install and use the package
+``` r
+install_github("nemesa/R-Packeges/PPBF")
+library(PPBF)
+
+parseResult<-PPBF.Run("c:/_WORK_/R/File/PrestoBlue_Sample.csv")
+print(parseResult$RowNum)
+print(parseResult$ColNum)
+print(parseResult$Data)
+```
+## Console result:
+```
+> library(PPBF)
+> parseResult<-PPBF.Run("c:/_WORK_/R/File/PrestoBlue_Sample.csv")
+> print(parseResult$RowNum)
+[1] 8
+> print(parseResult$ColNum)
+[1] 12
+> print(parseResult$Data)
+  X00 X01 X02 X03 X04 X05 X06 X07 X08 X09 X10 X11 X12
+1   A 101 102 103 104 105 106 107 108 109 110 111 112
+2   B 201 202 203 204 205 206 207 208 209 210 211 212
+3   C 301 302 303 304 305 306 307 308 309 310 311 312
+4   D 401 402 403 404 405 406 407 408 409 410 411 412
+5   E 501 502 503 504 505 506 507 508 509 510 511 512
+6   F 601 602 603 604 605 606 607 608 609 610 611 612
+7   G 701 702 703 704 705 706 707 708 709 710 711 712
+8   H 801 802 803 804 805 806 807 808 809 810 811 812
+```
+
+## Example file's content (PrestoBlue_Sample.csv):
+```
+Plate information 
+--- more text ---
+No background information available.
+Results for Meas A -  (RFU)
+,01,02,03,04,05,06,07,08,09,10,11,12,
+A,101,102,103,104,105,106,107,108,109,110,111,112,
+B,201,202,203,204,205,206,207,208,209,210,211,212,
+C,301,302,303,304,305,306,307,308,309,310,311,312,
+D,401,402,403,404,405,406,407,408,409,410,411,412,
+E,501,502,503,504,505,506,507,508,509,510,511,512,
+F,601,602,603,604,605,606,607,608,609,610,611,612,
+G,701,702,703,704,705,706,707,708,709,710,711,712,
+H,801,802,803,804,805,806,807,808,809,810,811,812,
+
+--- more text ---
+
+Number of rows,,,,8
+Number of columns,,,,12
+Number of the wells in the plate,,,,96
+Height of the plate,,,,14.35 mm
+Diameter of the well,,,,6.5 mm
+Volume of the well,,,,300 µl
+
+--- more text ---
+
+Exported with EnSpire Workstation version 4.10.3005.1440
+```
+
